76e5d526b8e5db5f7dcadfb14023b26fa5bddd5b
lee2963 <sodel2963@naver.com>
2023-07-12 15:15:51 +0900

    백준 1958(LCS 3)

---
+++ src/BJ_1958.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_1958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        String str3 = br.readLine();
+
+        int len1 = str1.length() + 1;
+        int len2 = str2.length() + 1;
+        int len3 = str3.length() + 1;
+
+        int[][][] dp = new int[len1][len2][len3];
+
+
+        for (int i = 1; i <len1; i++) {
+            for (int j = 1; j < len2; j++) {
+                for (int k = 1; k < len3; k++) {
+                    if (str1.charAt(i -1) == str2.charAt(j - 1) && str2.charAt(j - 1) == str3.charAt(k - 1)) {
+                        dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                    } else{
+                        dp[i][j][k] = Math.max(dp[i][j][k - 1], Math.max(dp[i - 1][j][k], dp[i][j - 1][k]));
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[len1 - 1][len2 - 1][len3 - 1]);
+    }
+}
