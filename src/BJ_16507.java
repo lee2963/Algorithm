93d17d2fb3b0cecb19a4428ba543953be0eaf4af
lee2963 <sodel2963@naver.com>
2023-07-20 21:20:03 +0900

    백준 16507(어두운 건 무서워)

---
+++ src/BJ_16507.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_16507 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[r + 1][c + 1];
+
+        for (int i = 1; i <= r; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= c; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i][j - 1] + arr[i - 1][j] - arr[i - 1][j - 1];
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int cnt = (x2 - x1 + 1) * (y2 - y1 + 1);
+
+            answer.append((arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1]) / cnt ).append("\n");
+        }
+
+        System.out.println(answer);
+    }
+}
