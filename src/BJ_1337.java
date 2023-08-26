a17cf5719a5be38f48af6b72f9abb707be2d2de3
lee2963 <sodel2963@naver.com>
2023-08-26 20:20:58 +0900

    백준 1337(올바른 배열)

---
+++ src/BJ_1337.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ_1337 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+            int cnt = 1;
+            for(int j = i + 1; j < n; j++){
+                if(arr[j] - arr[i] < 5){
+                    cnt++;
+                } else{
+                    break;
+                }
+            }
+            answer = Math.max(answer, cnt);
+        }
+        System.out.println(5 - answer);
+    }
+}
