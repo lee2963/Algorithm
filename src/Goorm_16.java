b4c02a9d64ca1f2b2d2c54ddf139c14951152d72
lee2963 <sodel2963@naver.com>
2023-09-04 14:51:47 +0900

    구름톤 챌린지 (연합)

--- src/Goorm_16.java
+++ src/Goorm_16.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 class Goorm_16 {
-    static List<Integer>[] group;
+    static boolean[][] bridge;
     static boolean[] visited;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,13 +11,8 @@ class Goorm_16 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        group = new ArrayList[n + 1];
         visited = new boolean[n + 1];
-        boolean[][] bridge = new boolean[n + 1][n + 1];
-
-        for(int i = 1; i <= n; i++){
-            group[i] = new ArrayList<>();
-        }
+        bridge = new boolean[n + 1][n + 1];
 
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
@@ -28,31 +23,23 @@ class Goorm_16 {
             bridge[from][to] = true;
         }
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(bridge[i][j] && bridge[j][i]){
-                    group[i].add(j);
-                    group[j].add(i);
-                }
-            }
-        }
-
         int answer = 0;
         for(int i = 1; i <= n; i++){
             if(!visited[i]){
-                visited[i] = true;
                 answer++;
-                dfs(i);
+                dfs(i, n);
             }
         }
         System.out.print(answer);
     }
-    static void dfs(int node){
+    static void dfs(int node, int n){
+        visited[node] = true;
 
-        for(Integer next : group[node]){
-            if(!visited[next]){
-                visited[next] = true;
-                dfs(next);
+        for(int i = 1; i <= n; i++){
+            if(!visited[i] && bridge[node][i]){
+                if(bridge[i][node]){
+                    dfs(i, n);
+                }
             }
         }
     }
